7a888fbb8f6a83839bf8cbf1ba8e1f4d5c8fe675
Md Asif <asif212327@gmail.com>
2026-01-23 00:12:37 +0530

    Add files via upload

---
+++ C_Beginner/18_Square_Cube.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+void main(){
+    int n;
+    scanf("%d",&n);
+    int square = n*n;
+    int cube = n*n*n;
+    printf("The square of %d is %d and the cube of %d is %d",n,square,n,cube);
+}
