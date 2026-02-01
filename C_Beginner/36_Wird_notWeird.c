41703b37e2b50390ac3f16115beb3a2b4c077d0a
Md Asif <asif212327@gmail.com>
2026-02-01 09:43:18 +0530

    Add files via upload

---
+++ C_Beginner/36_Wird_notWeird.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    if(n%2!=0)
+        printf("Weird");
+    else if(n>=2&&n<=5)
+        printf("Not Weird");
+    else if(n>=6&&n<=20)
+        printf("Weird");
+    else
+        printf("Not Weird");
+}
